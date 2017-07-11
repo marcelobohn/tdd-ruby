a9e4755d63542a70768ad92c42c938bf5fd8d8d9
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-07-10 21:07:34 -0300

    inclusão de novo teste

--- spec/soma_spec.rb
+++ spec/soma_spec.rb
@@ -3,5 +3,10 @@ require_relative '../soma'
 RSpec.describe "soma" do
   it "soma dois números" do
     expect(soma(2,2)).to eq(4)
+    expect(soma(4,6)).to eq(10)
+  end
+
+  it "soma sem parametros" do
+    expect(soma()).to eq(0)
   end
 end
